912a60829a28305b4c06fa7faaf90bb09cdbd9ce
Shawn Nehring <shawnnehring@gmail.com>
2026-08-14 16:46:07 -0500

    Delete ProverbsADay directory

--- ProverbsADay/detect.py
+++
@@ -1,8 +0,0 @@
-import chardet
-
-with open("ProverbsADay\\proverbs.txt", "rb") as f:
-    raw_data = f.read()
-    result = chardet.detect(raw_data)
-    detected_encoding = result['encoding']
-print(detected_encoding)
-#file = open("ProverbsADay\\proverbs.txt", encoding=detected_encoding)
--- ProverbsADay/read.py
+++
@@ -1,4 +0,0 @@
-file = open("frank.txt","r")
-lister = file.readlines()
-print(lister)
-file.close()
